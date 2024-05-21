4afa3ac5dcdd5449453705af6f8ff8f022e14048
youngil <youngil@placeb.com>
2024-05-21 16:44:06 +0900

    리액트에서 다시 렌더링하는 경우

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 export default function Counter() {
+    console.log("RE RENDERED!");
     const [count, setCount] = useState(0);
     const addOne = () => {
         setCount(count + 1);
+    }
+    const setToTen = () => {
+        setCount(10);
     }
     const addThree = () => {
         setCount(currentCount => currentCount + 1);
@@ -15,6 +19,8 @@ export default function Counter() {
             <p>Count: {count}</p>
             <button onClick={addOne}>+1</button>
             <button onClick={addThree}>+3</button>
+            <button onClick={setToTen}>set to 10</button>
+            
         </div>
     )
 
