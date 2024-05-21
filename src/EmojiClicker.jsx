cb1075f8e787170f949353aaf5566f3fdc0dbab9
정영일 <bbangwon@outlook.com>
2024-05-21 22:59:25 +0900

    리액트에서 배열의 요소를 삭제하는 방법

--- src/EmojiClicker.jsx
+++ src/EmojiClicker.jsx
@@ -1,14 +1,25 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
+
+function randomEmoji() {
+    const emojis = ["😊", "😂", "😍", "🥰", "😎", "😜"];
+    return emojis[Math.floor(Math.random() * emojis.length)];
+}
+
 export default function EmojiClicker() {
-    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😊"}]);
+    const [emojis, setEmojis] = useState([{ id: uuid(), emoji: randomEmoji() }]);
     const addEmoji = () => {
-        setEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: "😊"}]);
+        setEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: randomEmoji() }]);
+    }
+    const deleteEmoji = (id) => {
+        setEmojis((oldEmojis) => oldEmojis.filter((emoji) => emoji.id !== id));
     }
     return (
         <div>
             {emojis.map((emoji) => (
-            <span style={{fontSize: "4rem" }} key={emoji.id}>{emoji.emoji}</span>
+                <span onClick={() => deleteEmoji(emoji.id)}
+                    style={{ fontSize: "4rem" }}
+                    key={emoji.id}>{emoji.emoji}</span>
             ))}
             <button onClick={addEmoji}>Add Emoji</button>
         </div>
