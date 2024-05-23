55a109aa496807ce66bf890cbe112e58d09e8a2b
youngil <youngil@placeb.com>
2024-05-23 20:22:26 +0900

    함수를 프로퍼티로 전달

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,23 @@ import './App.css'
 //import ScoreKeeper2 from './ScoreKeeper2'
 // import Lucky7 from './Lucky7'
 import LuckyN from './LuckyN'
+import {sum} from './utils'
+
+function lessThan4(dice) {
+  return sum(dice) < 4;
+}
+
+function allSameValue(dice) {
+  return dice.every(d => d === dice[0]);
+}
+
 
 function App() {
 
   return (
     <>
-    <LuckyN />
-    <LuckyN numDice={3} goal={11} />
+    <LuckyN winCheck={lessThan4} title="Roll less than 4" />
+    <LuckyN winCheck={allSameValue} numDice={3} title="Roll the same number"/>
     {/* <Lucky7 /> */}
     {/* <ScoreKeeper2 numplayers={3} target={5} /> */}
     {/* <EmojiClicker /> */}
