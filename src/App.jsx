88909b0c0c27f33421ca4ed90054129ea622d349
youngil <youngil@placeb.com>
2024-05-23 20:10:57 +0900

    LuckyN 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,14 @@ import './App.css'
 //import EmojiClicker from './EmojiClicker'
 //import ScoreKeeper2 from './ScoreKeeper2'
 // import Lucky7 from './Lucky7'
-import Dice from './Dice'
+import LuckyN from './LuckyN'
 
 function App() {
 
   return (
     <>
-    <Dice dice={[6, 2, 3]} />
-    <Dice dice={[1, 2, 3]} color="red" />
-    <Dice dice={[1, 5, 4]} />
+    <LuckyN />
+    <LuckyN numDice={3} goal={11} />
     {/* <Lucky7 /> */}
     {/* <ScoreKeeper2 numplayers={3} target={5} /> */}
     {/* <EmojiClicker /> */}
