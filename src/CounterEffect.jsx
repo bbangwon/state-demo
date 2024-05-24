d8750fdde2252320e8e4006354acce9b055391dc
youngil <youngil@placeb.com>
2024-05-24 15:57:52 +0900

    useEffect 의존성

--- src/CounterEffect.jsx
+++ src/CounterEffect.jsx
@@ -2,15 +2,19 @@ import { useState, useEffect } from "react";
 
 export default function CounterEffect() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
     useEffect(function myEffect(){
         console.log("I'm in the effect!");
-    });
+    }, []);
     const increment = () => setCount(count + 1);
+    const handleChange = (e) => setName(e.target.value);
 
     return(
         <div>
             <h1>{count}</h1>
             <button onClick={increment}>Increment</button>
+            <p>{name}</p>
+            <input type="text" value={name} onChange={handleChange}/>
         </div>
     )
 }
